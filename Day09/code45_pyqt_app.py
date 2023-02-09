0baf609e4f5d69e4f17cfd0c5b767ca4bcc23c24
KOOJAIN <rnwkdls97@naver.com>
2023-02-09 17:28:48 +0900

    2월

---
+++ Day09/code45_pyqt_app.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+class MyApp(QWidget): #내가 MyApp이란 클래스를 만들건데 부모는 QWidget이야
+   
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        # GUI 화면 설정
+        self.setWindowTitle('Simple window')
+        # self.move(1920 // 2 - 200, 900 // 2 - 100)  # 띄우는 위치 조정(정중앙)
+        self.resize(400, 200)
+        self.show() # 핵심!!  # 파이썬 창이뜬다. 작업관리자에서 관리 활용하시길
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
